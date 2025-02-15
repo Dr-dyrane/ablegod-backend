e8f69293c75a6461c638b1a7712a220944f037e5
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2025-02-15 19:17:18 +0100

    modified:   api/index.js

--- api/index.js
+++ api/index.js
@@ -110,19 +110,6 @@ const propertyId = process.env.GA4_PROPERTY_ID;
 // Authenticate Service Account
 const authenticate = async () => {
 	const auth = new google.auth.GoogleAuth({
-		// credentials: {
-		// 	type: process.env.SA_TYPE,
-		// 	project_id: process.env.SA_PROJECT_ID,
-		// 	private_key_id: process.env.SA_PRIVATE_KEY_ID,
-		// 	private_key: process.env.SA_PRIVATE_KEY.replace(/\\n/g, "\n").trim(),
-		// 	client_email: process.env.SA_CLIENT_EMAIL,
-		// 	client_id: process.env.SA_CLIENT_ID,
-		// 	auth_uri: process.env.SA_AUTH_URI,
-		// 	token_uri: process.env.SA_TOKEN_URI,
-		// 	auth_provider_x509_cert_url: process.env.SA_AUTH_PROVIDER_CERT_URL,
-		// 	client_x509_cert_url: process.env.SA_CLIENT_CERT_URL,
-		// },
-		// keyFile: path.join(__dirname, 'service-account-file.json'),
 		credentials,
 		scopes: SCOPES,
 	});
